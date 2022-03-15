0e25b05a3c5a606a9a615db97d17677003863026
Arthur Meria <85205144+arthurmeira@users.noreply.github.com>
2022-03-14 23:00:11 -0300

    Update ex49.pas

--- ex49.pas
+++ ex49.pas
@@ -4,18 +4,18 @@ Var
 Begin
 	verificar := 0;
 	
-	write('Digite um número inteiro: ');
+	write('Digite um nÃºmero inteiro: ');
 	read(n);
 	
 	for i := 1 to n do 
-		begin
-			if (n mod i) = 0 then
-				verificar := verificar + 1;			
-		end;
+	begin
+		if (n mod i) = 0 then
+			verificar := verificar + 1;			
+	end;
 		
-		if (verificar = 2) or (verificar = 0) then
-			writeln(n,' é um número primo.')
-		else
-			writeln(n,' não é um número primo.');
-		readkey;
+	if (verificar = 2) or (verificar = 0) then
+		writeln(n,' Ã© um nÃºmero primo.')
+	else
+		writeln(n,' nÃ£o Ã© um nÃºmero primo.');
+	readkey;
 End.
